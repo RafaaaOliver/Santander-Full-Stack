1e227a7035b09c470b4ecf1ba51627e9022a3585
rafaellstos@hotmail.com <rafaellstos@hotmail.com>
2022-07-04 11:53:58 -0300

    update

--- Módulo II/JS/basic/contador/assets/scripts.js
+++ Módulo II/JS/basic/contador/assets/scripts.js
@@ -1,17 +1,26 @@
 let count = 0;
 
-const CURRENT_NUMBER = document.getElementById('currentNumber');
+let CURRENT_NUMBER = document.getElementById('currentNumber');
+console.log(CURRENT_NUMBER)
 
 function increment() {
 	count++;
+	if (count >= 0){
+		CURRENT_NUMBER.style.color = "black"
+	}
 	CURRENT_NUMBER.innerHTML = count;
 }
 
 function decrement() {
 	count--;
+	if (count < 0 ) {
+		CURRENT_NUMBER.style.color = "red"
+	}
 	CURRENT_NUMBER.innerHTML = count;
 }
 
-function test() {
-	kdowkdpo;
+function clean() {
+	CURRENT_NUMBER.innerHTML = 0;
+	count = 0
+	CURRENT_NUMBER.style.color = "black"
 }
